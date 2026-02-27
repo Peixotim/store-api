f8c6c3d9b85d195dd219f5f13adccb888736fe1e
Peixotim <pedropeixotovz@gmail.com>
2026-02-27 17:56:00 -0300

    feat: adicionar rotas de autenticação e verificação de saúde ao serviço de autenticação

--- services/auth-service/src/server.ts
+++ services/auth-service/src/server.ts
@@ -1,12 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv/config';
 import { errorHandler } from './middlewares/error-handler';
+import { Request, Response } from 'express';
+import { authRouter } from './routers/auth-router';
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(errorHandler);
+app.use('/auth',authRouter);
+
+app.get('/health', (req : Request , res : Response) => {
+  res.send(`API is Running`);
+});
 
 
 app.listen(port , () => {
