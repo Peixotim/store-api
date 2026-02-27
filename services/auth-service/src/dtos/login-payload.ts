b385404e173f880fd70b72329761c65288c4e939
Peixotim <pedropeixotovz@gmail.com>
2026-02-27 15:21:11 -0300

    feat: adicionar DTO LoginPayload e interface LoginReciveAPI para validação de login

---
+++ services/auth-service/src/dtos/login-payload.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsString , MinLength} from "class-validator";
+
+export class LoginPayload{
+    @IsEmail({}, { message: 'Invalid email format' })
+    email!: string;
+
+    @IsString()
+    @MinLength(8, { message: 'Password must be at least 8 characters' })
+    password!: string;
+}
+
+export interface LoginReciveAPI{
+    uuid : string;
+    name : string;
+    email : string;
+    cpf: string;
+    password: string;
+    zipcode : string;
+}
