8f2ae3f85dce07c7c72cdc3f5293235d5d56bf8d
Peixotim <pedropeixotovz@gmail.com>
2026-03-02 15:16:25 -0300

    feat: refatorar inicialização do servidor para usar função assíncrona e autenticação do sequelize

--- services/user-service/src/server.ts
+++ services/user-service/src/server.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import sequelize from './config/sequelize';
 
 const app = express();
 const port = process.env.PORT || 3002;
@@ -8,6 +9,18 @@ app.get('/', (req, res) => {
   res.send('Hello from user-service!');
 });
 
-app.listen(port, () => {
-  console.log(`user-service listening at http://localhost:${port}`);
-});
+async function bootstrap(): Promise<void> {
+  try {
+    await sequelize.authenticate();
+
+    app.listen(port, () => {
+      console.log(`user-service listening at http://localhost:${port}`);
+    });
+
+  } catch (error) {
+    console.error(`Error is ${error}`);
+    process.exit(1);
+  }
+}
+
+bootstrap();
