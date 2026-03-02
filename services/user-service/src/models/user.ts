c0b03cc38903b0cb66fb8135d81247b30759a3c7
Peixotim <pedropeixotovz@gmail.com>
2026-03-02 15:17:06 -0300

    feat: adicionar modelo User com atributos e validações

---
+++ services/user-service/src/models/user.ts
@@ -0,0 +1,62 @@
+import { DataTypes, Model } from 'sequelize';
+import type { UsersAttributes, UsersCreate } from '../dtos/user-dto';
+import sequelize from '../config/sequelize';
+
+class User extends Model<UsersAttributes, UsersCreate> implements UsersAttributes {
+  public id!: string;
+  public name!: string;
+  public cpf!: string;
+  public password!: string;
+  public zipcode!: string;
+  public email!: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    cpf: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        len: [11, 14],
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    zipcode: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'users',
+    timestamps: true,
+    underscored: true,
+  }
+);
+
+export default User;
