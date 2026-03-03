9dbb72d6ec2b4d3fb5b4f2bd5bb631c6490548a1
Peixotim <pedropeixotovz@gmail.com>
2026-03-03 15:08:04 -0300

    feat: adicionar middleware de validação de requisições

---
+++ services/user-service/src/middlewares/validate.ts
@@ -0,0 +1,22 @@
+// middlewares/validate.ts
+import { ZodTypeAny } from 'zod';
+import { Request, Response, NextFunction } from 'express';
+
+export const validate =
+  (schema: ZodTypeAny, property: 'body' | 'params' | 'query') =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req[property]);
+
+    if (!result.success) {
+      return res.status(400).json({
+        message: 'Validation error',
+        errors: result.error.issues.map(issue => ({
+          field: issue.path.join('.'),
+          message: issue.message,
+        })),
+      });
+    }
+
+    req[property] = result.data;
+    next();
+  };
