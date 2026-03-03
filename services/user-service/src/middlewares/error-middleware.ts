cfec130b2ec625165a14a73c5ae0a7ce5e2ff280
Peixotim <pedropeixotovz@gmail.com>
2026-03-03 15:07:37 -0300

    feat: adicionar middleware de tratamento de erros

---
+++ services/user-service/src/middlewares/error-middleware.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import { HttpError } from '../errors/http-errors';
+
+export function errorMiddleware(
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (error instanceof HttpError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    message: 'Internal server error',
+  });
+}
