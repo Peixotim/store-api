d3705a3b38b843b10b9d2c897a0fad04807be14f
Peixotim <pedropeixotovz@gmail.com>
2026-03-03 10:21:39 -0300

    refactor: ajustar formatação e adicionar métodos de busca no repositório de usuários

--- services/user-service/src/repositories/user-repository.ts
+++ services/user-service/src/repositories/user-repository.ts
@@ -1,20 +1,46 @@
-import { UsersCreate } from "../dtos/user-dto";
-import User from "../models/user";
+import { UsersCreate } from '../dtos/user-dto';
+import User from '../models/user';
 
-export class UserRepository{
-  public async create(data : UsersCreate):Promise<User>{
-    return await User.create({...data});
+export class UserRepository {
+  public async create(data: UsersCreate): Promise<User> {
+    return await User.create({ ...data });
   }
 
-  public async existsByCpf(cpf : string):Promise<boolean>{
-    const count = await User.count({where:{cpf}});
+  public async existsById(id: string): Promise<boolean> {
+    const count = await User.count({ where: { id } });
     return count > 0;
   }
 
-  public async existsByEmail(email : string):Promise<boolean>{
-    const count = await User.count({where:{email}});
+  public async existsByCpf(cpf: string): Promise<boolean> {
+    const count = await User.count({ where: { cpf } });
     return count > 0;
   }
 
-  //Criar os metodos depois de findBy...
+  public async existsByEmail(email: string): Promise<boolean> {
+    const count = await User.count({ where: { email } });
+    return count > 0;
+  }
+
+  public async findByMail(email: string): Promise<User | null> {
+    const user = await User.findOne({ where: { email } });
+    return user;
+  }
+
+  public async findByCpf(cpf: string): Promise<User | null> {
+    const user = await User.findOne({ where: { cpf } });
+    return user;
+  }
+
+  public async findAll(): Promise<User[]> {
+    return await User.findAll();
+  }
+
+  public async findById(id: string): Promise<User | null> {
+    return await User.findByPk(id);
+  }
+
+  public async desactive(id: string): Promise<boolean> {
+    const [affectedRows] = await User.update({ isActive: false }, { where: { id } });
+    return affectedRows > 0;
+  }
 }
