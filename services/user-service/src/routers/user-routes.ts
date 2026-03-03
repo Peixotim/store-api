0dfa5e631e32b8114a2e66aa38a9655a07ac6338
Peixotim <pedropeixotovz@gmail.com>
2026-03-03 15:08:23 -0300

    feat: adicionar rotas de usuário com validação de parâmetros

---
+++ services/user-service/src/routers/user-routes.ts
@@ -0,0 +1,52 @@
+import { Router } from 'express';
+import { UserController } from '../controllers/user-controller';
+import { validate } from '../middlewares/validate';
+import {
+  createUserSchema,
+  idParamSchema,
+  emailParamSchema,
+  cpfParamSchema,
+} from '../dtos/user-schema';
+
+const router : Router = Router();
+const controller = new UserController();
+
+router.post(
+  '/',
+  validate(createUserSchema, 'body'),
+  controller.create.bind(controller)
+);
+
+router.get('/health',controller.health.bind(controller));
+
+router.get(
+  '/',
+  controller.findAll.bind(controller)
+);
+
+router.get(
+  '/id/:id',
+  validate(idParamSchema, 'params'),
+  controller.findById.bind(controller)
+);
+
+router.get(
+  '/email/:email',
+  validate(emailParamSchema, 'params'),
+  controller.findByEmail.bind(controller)
+);
+
+router.get(
+  '/cpf/:cpf',
+  validate(cpfParamSchema, 'params'),
+  controller.findByCpf.bind(controller)
+);
+
+router.patch(
+  '/desactive/:id',
+  validate(idParamSchema, 'params'),
+  controller.desactive.bind(controller)
+);
+
+
+export default router;
