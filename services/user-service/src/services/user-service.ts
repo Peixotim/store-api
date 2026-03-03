2ae749492422770cb5a037a8af60a72a290844c4
Peixotim <pedropeixotovz@gmail.com>
2026-03-03 10:21:45 -0300

    feat: adicionar métodos de busca e tratamento de erros no serviço de usuários

--- services/user-service/src/services/user-service.ts
+++ services/user-service/src/services/user-service.ts
@@ -1,53 +1,128 @@
-import { UsersCreate } from "../dtos/user-dto";
-import { BadRequestError, ConflictError, HttpError, InternalServerError } from "../errors/http-errors";
-import { UserRepository } from "../repositories/user-repository";
-import { UniqueConstraintError } from "sequelize";
-export class UserService{
+import { UsersCreate } from '../dtos/user-dto';
+import {
+  BadRequestError,
+  ConflictError,
+  HttpError,
+  InternalServerError,
+  NotFoundError,
+} from '../errors/http-errors';
+import User from '../models/user';
+import { UserRepository } from '../repositories/user-repository';
+import { UniqueConstraintError } from 'sequelize';
 
-  private userRepository = new UserRepository();
+export class UserService {
+  private readonly userRepository = new UserRepository();
 
-  public async create(payload : UsersCreate){
-    
+  public async findAll(): Promise<User[] | null> {
+    try {
+      return await this.userRepository.findAll();
+    } catch (error) {
+      if (error instanceof HttpError) throw error;
+      throw new InternalServerError(`Unexpected error: ${error}`);
+    }
+  }
+
+  public async create(payload: UsersCreate): Promise<User> {
     if (!payload || Object.keys(payload).length === 0) {
-      throw new BadRequestError(`Payload is empty!`);
-    }
-    if(await this.existsByCpf(payload.cpf)){
-      throw new ConflictError(`A user with this CPF already exists.`);
-    }
-      
-    if(await this.existsByEmail(payload.email)){
-      throw new ConflictError(`A user with this email already exists.`);
+      throw new BadRequestError('Payload is empty!');
     }
 
-    try{
-      const createUser = await this.userRepository.create(payload);
-      return createUser;
-    }catch(error){
+    if (await this.existsByCpf(payload.cpf)) {
+      throw new ConflictError('A user with this CPF already exists.');
+    }
+
+    if (await this.existsByEmail(payload.email)) {
+      throw new ConflictError('A user with this email already exists.');
+    }
+
+    try {
+      return await this.userRepository.create(payload);
+    } catch (error) {
       if (error instanceof HttpError) throw error;
 
       if (error instanceof UniqueConstraintError) {
-        throw new ConflictError(`Database constraint violation: Data already exists.`);
+        throw new ConflictError('Database constraint violation: Data already exists.');
       }
-      
-      throw new InternalServerError(`Unexpected error on register: ${error}`);
+
+      throw new InternalServerError(`Unexpected error: ${error}`);
     }
   }
 
-  public async existsByCpf(cpf : string){
-    try{
+  public async existsByCpf(cpf: string): Promise<boolean> {
+    try {
       return await this.userRepository.existsByCpf(cpf);
-    }catch(error){
+    } catch (error) {
       if (error instanceof HttpError) throw error;
-      throw new InternalServerError(`Unexpected error on register: ${error}`);
-    }
-    }
-  
-    public async existsByEmail(email : string){
-      try{
-        return await this.userRepository.existsByEmail(email);
-      }catch(error){
-      if (error instanceof HttpError) throw error;
-      throw new InternalServerError(`Unexpected error on register: ${error}`);
-      }
+      throw new InternalServerError(`Unexpected error: ${error}`);
     }
   }
+
+  public async existsByEmail(email: string): Promise<boolean> {
+    try {
+      return await this.userRepository.existsByEmail(email);
+    } catch (error) {
+      if (error instanceof HttpError) throw error;
+      throw new InternalServerError(`Unexpected error: ${error}`);
+    }
+  }
+
+  public async findByEmail(email: string): Promise<User> {
+    try {
+      const user = await this.userRepository.findByMail(email);
+
+      if (!user) {
+        throw new NotFoundError('User not found');
+      }
+
+      return user;
+    } catch (error) {
+      if (error instanceof HttpError) throw error;
+      throw new InternalServerError(`Unexpected error: ${error}`);
+    }
+  }
+
+  public async findByCpf(cpf: string): Promise<User> {
+    try {
+      const user = await this.userRepository.findByCpf(cpf);
+
+      if (!user) {
+        throw new NotFoundError('User not found');
+      }
+
+      return user;
+    } catch (error) {
+      if (error instanceof HttpError) throw error;
+      throw new InternalServerError(`Unexpected error: ${error}`);
+    }
+  }
+
+  public async findById(id: string): Promise<User> {
+    try {
+      const user = await this.userRepository.findById(id);
+
+      if (!user) {
+        throw new NotFoundError('User not found');
+      }
+
+      return user;
+    } catch (error) {
+      if (error instanceof HttpError) throw error;
+      throw new InternalServerError(`Unexpected error: ${error}`);
+    }
+  }
+
+  public async desactive(id: string): Promise<boolean> {
+    try {
+      const exists = await this.userRepository.existsById(id);
+
+      if (!exists) {
+        throw new NotFoundError('User not found');
+      }
+
+      return await this.userRepository.desactive(id);
+    } catch (error) {
+      if (error instanceof HttpError) throw error;
+      throw new InternalServerError(`Unexpected error: ${error}`);
+    }
+  }
+}
