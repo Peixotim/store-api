d1f240e7991c28ad412d254cab9761231e22e163
Peixotim <pedropeixotovz@gmail.com>
2026-03-03 10:21:09 -0300

    feat: adicionar chamada para runMigrations no bootstrap do serviço de usuário

--- services/user-service/src/server.ts
+++ services/user-service/src/server.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import sequelize from './config/sequelize';
+import { runMigrations } from './database/migrate';
 
 const app = express();
 const port = process.env.PORT || 3002;
@@ -12,11 +13,11 @@ app.get('/', (req, res) => {
 async function bootstrap(): Promise<void> {
   try {
     await sequelize.authenticate();
+    await runMigrations();
 
     app.listen(port, () => {
       console.log(`user-service listening at http://localhost:${port}`);
     });
-
   } catch (error) {
     console.error(`Error is ${error}`);
     process.exit(1);
