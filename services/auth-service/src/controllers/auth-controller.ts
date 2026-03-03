2a2f5ccc5c646d7ff43fbb6533771c811867e830
Peixotim <pedropeixotovz@gmail.com>
2026-03-03 10:20:49 -0300

    refactor: reorganizar importações e melhorar a formatação do código no AuthController

--- services/auth-service/src/controllers/auth-controller.ts
+++ services/auth-service/src/controllers/auth-controller.ts
@@ -1,35 +1,35 @@
-import { AuthService } from "../services/auth-service";
-import { NextFunction, Request, Response } from "express";
-export class AuthController{
-  private readonly authService = new AuthService();
+  import { AuthService } from "../services/auth-service";
+  import { NextFunction, Request, Response } from "express";
+  export class AuthController{
+    private readonly authService = new AuthService();
 
- public async login(req : Request , res : Response,next: NextFunction){
-    try{
-    const tokens = await this.authService.login(req.body);
-    return res.status(200).json(tokens);
-  }catch(error){
-    next(error);
-  }
-  }
-
-  public async register(req: Request , res: Response , next : NextFunction){
-    try{
-      await this.authService.register(req.body);
-      return res.status(201).json({
-        message: `User registered successfully`
-      })
-    }catch(error){
-      next(error)
-    }
-  }
-
-  public async refresh(req: Request, res : Response, next : NextFunction){
-    try{
-      const { refreshToken } = req.body;
-      const tokens = await this.authService.refresh(refreshToken);
+  public async login(req : Request , res : Response,next: NextFunction){
+      try{
+      const tokens = await this.authService.login(req.body);
       return res.status(200).json(tokens);
     }catch(error){
       next(error);
     }
+    }
+
+    public async register(req: Request , res: Response , next : NextFunction){
+      try{
+        await this.authService.register(req.body);
+        return res.status(201).json({
+          message: `User registered successfully`
+        })
+      }catch(error){
+        next(error)
+      }
+    }
+
+    public async refresh(req: Request, res : Response, next : NextFunction){
+      try{
+        const { refreshToken } = req.body;
+        const tokens = await this.authService.refresh(refreshToken);
+        return res.status(200).json(tokens);
+      }catch(error){
+        next(error);
+      }
+    }
   }
-}
