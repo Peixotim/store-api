5099a1625a090753a2a2104cbbd424396e9dd13a
Peixotim <pedropeixotovz@gmail.com>
2026-03-03 10:21:13 -0300

    fix: ajustar formatação e estilo no arquivo sequelize.ts

--- services/user-service/src/config/sequelize.ts
+++ services/user-service/src/config/sequelize.ts
@@ -1,4 +1,4 @@
-import { Options, Sequelize } from "sequelize";
+import { Options, Sequelize } from 'sequelize';
 import 'dotenv/config';
 
 const requiredEnvs = ['DB_NAME', 'DB_USER', 'DB_PASSWORD', 'DB_HOST'];
@@ -8,34 +8,33 @@ requiredEnvs.forEach((envVar) => {
   }
 });
 
-
-const dbOptions : Options = {
+const dbOptions: Options = {
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT) || 5432,
   dialect: 'postgres',
   logging: process.env.NODE_ENV === 'development' ? console.log : false,
-  
+
   pool: {
     max: 100,
-    min: 0 ,
+    min: 0,
     acquire: 30000,
-    idle: 10000
+    idle: 10000,
   },
 
-  define:{
+  define: {
     timestamps: true,
     underscored: true,
     freezeTableName: true,
   },
 
-  timezone: '-03:00'
-}
+  timezone: '-03:00',
+};
 
-const sequelize  = new Sequelize(
+const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USER as string,
   process.env.DB_PASSWORD as string,
-  dbOptions
-)
+  dbOptions,
+);
 
 export default sequelize;
