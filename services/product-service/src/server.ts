d1d7fc3654dbb244844c666da8f410b2b34e0159
Peixotim <pedropeixotovz@gmail.com>
2026-03-05 17:07:29 -0300

    feat: configurar o servidor para usar rotas e autenticação do Sequelize

--- services/product-service/src/server.ts
+++ services/product-service/src/server.ts
@@ -1,13 +1,28 @@
 import 'dotenv/config';
 import express from 'express';
+import router from './routes/product-routes';
+import sequelize from './config/sequelize';
+import { runMigrations } from './database/migrate';
 
 const app = express();
 const port = process.env.PORT || 3003;
 
+app.use(express.json());
+app.use('', router)
 app.get('/', (req, res) => {
   res.send('Hello from product-service!');
 });
 
+async function bootStrap(){
+  try{
+    await sequelize.authenticate();
+    await runMigrations();
 app.listen(port, () => {
   console.log(`product-service listening at http://localhost:${port}`);
 });
+  }catch(error){
+    console.error(`Error is ${error}`);
+    process.exit(1);
+  }
+}
+
