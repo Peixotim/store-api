aa162139611327d94c6cb58d269ef0ef4d0f3d08
Peixotim <pedropeixotovz@gmail.com>
2026-03-06 14:55:27 -0300

    fix: corrigir formatação de strings no arquivo register-payload.ts

--- services/auth-service/src/dtos/register-payload.ts
+++ services/auth-service/src/dtos/register-payload.ts
@@ -1,20 +1,20 @@
-import { IsEmail, IsString, MinLength, IsPostalCode } from 'class-validator';
+import { IsEmail, IsString, MinLength, IsPostalCode } from "class-validator";
 
 export class RegisterPayload {
-  @IsString({ message: 'Name is required' })
+  @IsString({ message: "Name is required" })
   name!: string;
 
-  @IsEmail({}, { message: 'Invalid email format' })
+  @IsEmail({}, { message: "Invalid email format" })
   email!: string;
 
   @IsString()
-  @MinLength(11, { message: 'CPF must have 11 digits' })
+  @MinLength(11, { message: "CPF must have 11 digits" })
   cpf!: string;
 
   @IsString()
-  @MinLength(8, { message: 'Password must be at least 8 characters' })
+  @MinLength(8, { message: "Password must be at least 8 characters" })
   password!: string;
 
-  @IsPostalCode('BR', { message: 'Invalid zipcode' })
+  @IsPostalCode("BR", { message: "Invalid zipcode" })
   zipcode!: string;
 }
