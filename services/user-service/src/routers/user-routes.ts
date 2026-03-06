0c643fda48629e75f4d59d4e33b638b7c3e4aa4a
Peixotim <pedropeixotovz@gmail.com>
2026-03-06 16:50:46 -0300

    style: ajustar a formatação do código no arquivo user-routes.ts

--- services/user-service/src/routers/user-routes.ts
+++ services/user-service/src/routers/user-routes.ts
@@ -2,10 +2,10 @@ import { Router } from 'express';
 import { UserController } from '../controllers/user-controller';
 import { validate } from '../middlewares/validate';
 import {
-    createUserSchema,
-    idParamSchema,
-    emailParamSchema,
-    cpfParamSchema,
+  createUserSchema,
+  idParamSchema,
+  emailParamSchema,
+  cpfParamSchema,
 } from '../dtos/user-schema';
 
 const router: Router = Router();
@@ -20,29 +20,29 @@ router.get('/', controller.findAll.bind(controller));
 router.get('/id/:id', validate(idParamSchema, 'params'), controller.findById.bind(controller));
 
 router.get(
-    '/email/:email',
-    validate(emailParamSchema, 'params'),
-    controller.findByEmail.bind(controller),
+  '/email/:email',
+  validate(emailParamSchema, 'params'),
+  controller.findByEmail.bind(controller),
 );
 
 router.get(
-    '/existsByEmail',
-    validate(emailParamSchema, 'query'),
-    controller.existsByEmail.bind(controller),
+  '/existsByEmail',
+  validate(emailParamSchema, 'query'),
+  controller.existsByEmail.bind(controller),
 );
 
 router.get(
-    '/existsByCpf',
-    validate(cpfParamSchema, 'query'),
-    controller.existsByCpf.bind(controller),
+  '/existsByCpf',
+  validate(cpfParamSchema, 'query'),
+  controller.existsByCpf.bind(controller),
 );
 
 router.get('/cpf/:cpf', validate(cpfParamSchema, 'params'), controller.findByCpf.bind(controller));
 
 router.patch(
-    '/desactive/:id',
-    validate(idParamSchema, 'params'),
-    controller.desactive.bind(controller),
+  '/desactive/:id',
+  validate(idParamSchema, 'params'),
+  controller.desactive.bind(controller),
 );
 
 export default router;
