cd52d9dba62bf5ea777ac35601f56ad6d2d6e373
Peixotim <pedropeixotovz@gmail.com>
2026-03-06 14:56:06 -0300

    fix: corrigir formatação no arquivo migrate.ts

--- services/user-service/src/database/migrate.ts
+++ services/user-service/src/database/migrate.ts
@@ -4,9 +4,9 @@ import sequelize from '../config/sequelize';
 import path from 'path';
 
 export const migrator = new Umzug({
-migrations: {
-  glob: path.join(__dirname, '../migrations/*.js'),
-},
+  migrations: {
+    glob: path.join(__dirname, '../migrations/*.js'),
+  },
   context: sequelize.getQueryInterface(),
   storage: new SequelizeStorage({ sequelize }),
   logger: console,
