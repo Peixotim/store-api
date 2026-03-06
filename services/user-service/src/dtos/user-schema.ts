2c9572820c297cfe789faabe362a91969a6c2683
Peixotim <pedropeixotovz@gmail.com>
2026-03-06 14:56:09 -0300

    fix: corrigir formatação e espaçamento no user-schema.ts

--- services/user-service/src/dtos/user-schema.ts
+++ services/user-service/src/dtos/user-schema.ts
@@ -1,12 +1,12 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const createUserSchema = z.object({
-  name : z.string().min(3),
-  email : z.string().email(),
-  password : z.string(),
-  cpf : z.string().length(11),
-  zipcode : z.string(),
-})
+  name: z.string().min(3),
+  email: z.string().email(),
+  password: z.string(),
+  cpf: z.string().length(11),
+  zipcode: z.string(),
+});
 
 export const idParamSchema = z.object({
   id: z.string().uuid(),
@@ -20,4 +20,4 @@ export const cpfParamSchema = z.object({
   cpf: z.string().min(11).max(14),
 });
 
-export type CreateUserInput = z.infer<typeof createUserSchema>
+export type CreateUserInput = z.infer<typeof createUserSchema>;
