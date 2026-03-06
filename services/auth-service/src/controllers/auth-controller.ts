fb74ce086f62f7b042a2c99697c155231f62ce1a
Peixotim <pedropeixotovz@gmail.com>
2026-03-06 14:55:22 -0300

    fix: corrigir formatação e espaçamento no controlador de autenticação

--- services/auth-service/src/controllers/auth-controller.ts
+++ services/auth-service/src/controllers/auth-controller.ts
@@ -1,41 +1,41 @@
-  import { AuthService } from "../services/auth-service";
-  import { NextFunction, Request, Response } from "express";
-  export class AuthController{
+import { AuthService } from "../services/auth-service";
+import { NextFunction, Request, Response } from "express";
+export class AuthController {
+  private authService = new AuthService();
 
-    private authService = new AuthService();
+  public async health(req: Request, res: Response) {
+    return res.status(200).json({
+      message: `API IS RUNNING !`,
+    });
+  }
 
-      public async health(req: Request, res: Response){
-        return res.status(200).json({
-          message: `API IS RUNNING !`
-        })
-      }
-  public async login(req : Request , res : Response,next: NextFunction){
-      try{
+  public async login(req: Request, res: Response, next: NextFunction) {
+    try {
       const tokens = await this.authService.login(req.body);
       return res.status(200).json(tokens);
-    }catch(error){
+    } catch (error) {
       next(error);
     }
-    }
+  }
 
-    public async register(req: Request , res: Response , next : NextFunction){
-      try{
-        await this.authService.register(req.body);
-        return res.status(201).json({
-          message: `User registered successfully`
-        })
-      }catch(error){
-        next(error)
-      }
-    }
-
-    public async refresh(req: Request, res : Response, next : NextFunction){
-      try{
-        const { refreshToken } = req.body;
-        const tokens = await this.authService.refresh(refreshToken);
-        return res.status(200).json(tokens);
-      }catch(error){
-        next(error);
-      }
+  public async register(req: Request, res: Response, next: NextFunction) {
+    try {
+      await this.authService.register(req.body);
+      return res.status(201).json({
+        message: `User registered successfully`,
+      });
+    } catch (error) {
+      next(error);
     }
   }
+
+  public async refresh(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { refreshToken } = req.body;
+      const tokens = await this.authService.refresh(refreshToken);
+      return res.status(200).json(tokens);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
