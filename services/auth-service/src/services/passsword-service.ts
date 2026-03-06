e275369d22da974ce1e3d050024de20d781304d0
Peixotim <pedropeixotovz@gmail.com>
2026-03-06 14:55:52 -0300

    fix: corrigir formatação e espaçamento no arquivo passsword-service.ts

--- services/auth-service/src/services/passsword-service.ts
+++ services/auth-service/src/services/passsword-service.ts
@@ -1,24 +1,23 @@
-import * as argon2 from 'argon2';
+import * as argon2 from "argon2";
 
-export class PasswordService{
-
-  public async hash(password : string):Promise<string>{
-    try{
-        return await argon2.hash(password,{
-          type: argon2.argon2id,
-          memoryCost: 2 ** 16,
-          timeCost: 3,
-          parallelism: 1,
-        })
-    }catch(error){
+export class PasswordService {
+  public async hash(password: string): Promise<string> {
+    try {
+      return await argon2.hash(password, {
+        type: argon2.argon2id,
+        memoryCost: 2 ** 16,
+        timeCost: 3,
+        parallelism: 1,
+      });
+    } catch (error) {
       throw new Error(`Error hashing password: ${error}`);
     }
   }
 
-  public async verify(hash: string,password:string):Promise<boolean>{
-    try{
-      return await argon2.verify(hash,password);
-    }catch(error){
+  public async verify(hash: string, password: string): Promise<boolean> {
+    try {
+      return await argon2.verify(hash, password);
+    } catch (error) {
       throw new Error(`Error verifying password: ${error}`);
     }
   }
