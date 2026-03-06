4b8af115853e814200d8532c759b5123f3652114
Peixotim <pedropeixotovz@gmail.com>
2026-03-06 14:55:42 -0300

    fix: corrigir formatação e espaçamento no arquivo auth-router.ts

--- services/auth-service/src/routers/auth-router.ts
+++ services/auth-service/src/routers/auth-router.ts
@@ -7,9 +7,17 @@ import { RegisterPayload } from "../dtos/register-payload";
 const router: Router = Router();
 const controller = new AuthController();
 
-router.post('/login', validateBody(LoginPayload), controller.login.bind(controller));
-router.post('/register', validateBody(RegisterPayload), controller.register.bind(controller));
-router.post('/refresh', controller.refresh.bind(controller));
-router.get('/health', controller.health.bind(controller));
+router.post(
+  "/login",
+  validateBody(LoginPayload),
+  controller.login.bind(controller),
+);
+router.post(
+  "/register",
+  validateBody(RegisterPayload),
+  controller.register.bind(controller),
+);
+router.post("/refresh", controller.refresh.bind(controller));
+router.get("/health", controller.health.bind(controller));
 
-export {router as authRouter}
+export { router as authRouter };
