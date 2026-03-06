63e2fd0e38b355a742ab6909dff3ade972bf6236
Peixotim <pedropeixotovz@gmail.com>
2026-03-06 14:55:25 -0300

    fix: corrigir formatação e espaçamento no arquivo login-payload.ts

--- services/auth-service/src/dtos/login-payload.ts
+++ services/auth-service/src/dtos/login-payload.ts
@@ -1,20 +1,20 @@
-import { IsEmail, IsString , MinLength} from "class-validator";
+import { IsEmail, IsString, MinLength } from "class-validator";
 
-export class LoginPayload{
-    @IsEmail({}, { message: 'Invalid email format' })
-    email!: string;
+export class LoginPayload {
+  @IsEmail({}, { message: "Invalid email format" })
+  email!: string;
 
-    @IsString()
-    @MinLength(8, { message: 'Password must be at least 8 characters' })
-    password!: string;
+  @IsString()
+  @MinLength(8, { message: "Password must be at least 8 characters" })
+  password!: string;
 }
 
-export interface LoginReciveAPI{
-    id : string;
-    name : string;
-    email : string;
-    cpf: string;
-    password: string;
-    zipcode : string;
-    isActive : boolean;
+export interface LoginReciveAPI {
+  id: string;
+  name: string;
+  email: string;
+  cpf: string;
+  password: string;
+  zipcode: string;
+  isActive: boolean;
 }
