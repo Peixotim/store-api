d4ada58a95d0020b76b658e0ac356b0047660f89
Peixotim <pedropeixotovz@gmail.com>
2026-03-06 14:56:13 -0300

    fix: corrigir formatação e remover linhas desnecessárias no error-middleware.ts

--- services/user-service/src/middlewares/error-middleware.ts
+++ services/user-service/src/middlewares/error-middleware.ts
@@ -1,12 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { HttpError } from '../errors/http-errors';
 
-export function errorMiddleware(
-  error: any,
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
+export function errorMiddleware(error: any, req: Request, res: Response, next: NextFunction) {
   if (error instanceof HttpError) {
     return res.status(error.statusCode).json({
       message: error.message,
