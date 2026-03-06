c39d160c73443daca5dfb1f2d302d6fa238730c8
Peixotim <pedropeixotovz@gmail.com>
2026-03-06 16:50:40 -0300

    feat: reorganizar rotas de produtos e adicionar validações

--- services/product-service/src/routes/product-routes.ts
+++ services/product-service/src/routes/product-routes.ts
@@ -1,7 +1,21 @@
-import { Router } from "express";
-
+import { Router } from 'express';
+import { ProductController } from '../controller/product-controller';
+import { validate } from '../middlewares/validate';
+import { createProduct, findSku, findById } from '../dtos/product-schema';
 const router: Router = Router();
+const controller = new ProductController();
 
+router.get('/', controller.findAll.bind(controller));
+router.post('/', validate(createProduct, 'body'), controller.create.bind(controller));
 
+router.get('/sku/:sku', validate(findSku, 'params'), controller.findBySku.bind(controller));
+router.get('/id/:id', validate(findById, 'params'), controller.findById.bind(controller));
+router.get('/exists/:sku', validate(findSku, 'params'), controller.existsInStock.bind(controller));
+
+router.delete(
+  '/stock/:sku',
+  validate(findSku, 'params'),
+  controller.removeInStock.bind(controller),
+);
 
 export default router;
