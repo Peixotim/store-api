2a8e53b4da549142ba09c8fcba7acc3cffc5cd6a
Peixotim <pedropeixotovz@gmail.com>
2026-03-10 16:43:55 -0300

    feat: adicionar DTO para atributos de pedidos

---
+++ services/order-service/src/dtos/order-dto.ts
@@ -0,0 +1,11 @@
+import { Optional } from 'sequelize';
+import { status } from '../enums/order-status';
+
+export interface OrderAttributes {
+  id: string;
+  user_id: string;
+  status: status;
+  total_price: number;
+}
+
+export interface OrderCreate extends Optional<OrderAttributes, 'id'> {}
