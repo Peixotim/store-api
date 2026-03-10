80bfe1e490977b81a960cb687146a7ba4dbe3d60
Peixotim <pedropeixotovz@gmail.com>
2026-03-10 16:44:33 -0300

    refactor: ajustar formatação do objeto de migrações no serviço de produtos

--- services/product-service/src/database/migrate.ts
+++ services/product-service/src/database/migrate.ts
@@ -1,12 +1,11 @@
-import { Sequelize } from 'sequelize';
 import { Umzug, SequelizeStorage } from 'umzug';
 import sequelize from '../config/sequelize';
 import path from 'path';
 
 export const migrator = new Umzug({
-migrations: {
-  glob: path.join(__dirname, '../migrations/*.js'),
-},
+  migrations: {
+    glob: path.join(__dirname, '../migrations/*.js'),
+  },
   context: sequelize.getQueryInterface(),
   storage: new SequelizeStorage({ sequelize }),
   logger: console,
