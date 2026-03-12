e3e2216866e1ad6bc69d15822e2ea6850ab3f773
Peixotim <pedropeixotovz@gmail.com>
2026-03-12 14:03:52 -0300

    feat: adicionar injeção de dependência do OrderService no OrderController

--- services/order-service/src/controllers/order-controller.ts
+++ services/order-service/src/controllers/order-controller.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from 'express';
+import { OrderService } from '../services/order-service';
 export class OrderController {
+  constructor(private orderService: OrderService) {}
   public async health(req: Request, res: Response) {
     return res.status(200).json({
       message: `API IS RUNNING !`,
