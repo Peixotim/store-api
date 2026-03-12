4d675d7d18572f9ee489d85966ccf114ef7c3a62
Peixotim <pedropeixotovz@gmail.com>
2026-03-12 14:04:00 -0300

    feat: adicionar método orderUpdate ao OrderRepository

--- services/order-service/src/repositories/order-repository.ts
+++ services/order-service/src/repositories/order-repository.ts
@@ -1,10 +1,15 @@
 import { OrderCreateDTO, OrderItemCreateDTO } from '../dtos/ order-dto-create';
 import Order from '../models/order';
 import OrderItem from '../models/orderItem';
+import { OrderOptional } from '../dtos/order-dto';
 
 export class OrderRepository {
   public async create(request: OrderCreateDTO) {
     return await Order.create({ ...request });
+  }
+
+  public async orderUpdate(orderId: string, schema: OrderOptional) {
+    return await Order.update(schema, { where: { id: orderId } });
   }
 }
 
