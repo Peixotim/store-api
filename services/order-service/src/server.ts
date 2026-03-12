35317ef4d67ef288ea0f6ba35848b0f65ca9440a
Peixotim <pedropeixotovz@gmail.com>
2026-03-12 14:03:50 -0300

    feat: integrar serviço RabbitMQ ao microserviço de pedidos

--- services/order-service/src/server.ts
+++ services/order-service/src/server.ts
@@ -3,9 +3,12 @@ import express from 'express';
 import router from './routes/order-routes';
 import sequelize from './config/squelize';
 import { runMigrations } from './database/migrate';
+import { RabbitMQService } from './services/rabbitmq-service';
 
 const app = express();
 const port = process.env.PORT || 3004;
+const amqpUrl = process.env.AMQP_URL || 'amqp://guest:guest@rabbitmq:5672';
+const rabbitMqService = new RabbitMQService(amqpUrl);
 
 app.use(express.json());
 app.use('', router);
@@ -14,6 +17,8 @@ async function bootStrap() {
   try {
     await sequelize.authenticate();
     await runMigrations();
+    await rabbitMqService.start();
+
     app.listen(port, () => {
       console.log(`Microservice Order is running !`);
     });
