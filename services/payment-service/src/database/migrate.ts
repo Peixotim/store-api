def27fbf17d12e1008e2e7224f5b7a434398049f
Peixotim <pedropeixotovz@gmail.com>
2026-03-16 11:16:30 -0300

    feat: adicionar script de migrações utilizando Umzug e Sequelize

---
+++ services/payment-service/src/database/migrate.ts
@@ -0,0 +1,16 @@
+import { Umzug, SequelizeStorage } from 'umzug';
+import sequelize from '../config/sequelize';
+import path from 'path';
+
+export const migrator = new Umzug({
+  migrations: {
+    glob: path.join(__dirname, '../migrations/*.js'),
+  },
+  context: sequelize.getQueryInterface(),
+  storage: new SequelizeStorage({ sequelize }),
+  logger: console,
+});
+
+export async function runMigrations() {
+  await migrator.up();
+}
