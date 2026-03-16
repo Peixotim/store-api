7b84ebd3816c0ed2c7e37c445c1a690c6f2ac18f
Peixotim <pedropeixotovz@gmail.com>
2026-03-16 11:16:10 -0300

    refactor: atualizar importações e melhorar a estrutura do RabbitMQService

--- services/order-service/src/services/rabbitmq-service.ts
+++ services/order-service/src/services/rabbitmq-service.ts
@@ -1,43 +1,53 @@
-import { Connection, Channel, connect, Message } from 'amqplib';
+import * as amqp from 'amqplib';
+
 export class RabbitMQService {
-  private connection!: Connection;
-  private channel!: Channel;
+  private connection!: amqp.Connection;
+  private channel!: amqp.Channel;
 
   constructor(private uri: string) {}
 
-  private reconnect(): void {
-    setTimeout(() => {
-      this.start();
-    }, 5000);
-  }
-
   public async start(): Promise<void> {
     try {
-      this.connection = await connect(this.uri);
+      this.connection = await amqp.connect(this.uri);
       this.channel = await this.connection.createChannel();
-      console.log('[RabbitMQ] Successfully connected!');
 
-      this.connection.on('error', (error) => {
-        console.error(`[RabbitMQ] Error in connection`, error);
-      });
-
-      this.connection.on('close', () => {
-        console.warn('[RabbitMQ] Connection lost. Attempting to reconnect in 5 seconds...');
-        this.reconnect();
-      });
+      console.log(`[RabbitMQ] Connected and channel created!`);
     } catch (error) {
-      console.error(`Error in Start Connection with RabbitMQ`);
+      console.error(`[RabbitMQ] Errors related to connecting to RabbitMQ:`, error);
       throw error;
     }
   }
-
-  public async publishInQueue<T>(queue: string, message: T): Promise<void> {
+  public async consume<T>(queue: string, callback: (message: T) => Promise<void>): Promise<void> {
     if (!this.channel) {
-      throw new Error(`RabbitMQ Channel it is not open !`);
+      throw new Error(`[RabbitMQ] Channel not initialized`);
     }
 
     await this.channel.assertQueue(queue, { durable: true });
 
+    await this.channel.prefetch(1);
+
+    console.log(`[RabbitMQ] Listening to the queue: ${queue}`);
+
+    this.channel.consume(queue, async (msg) => {
+      if (msg) {
+        try {
+          const content: T = JSON.parse(msg.content.toString());
+          await callback(content);
+
+          this.channel.ack(msg);
+        } catch (error) {
+          console.error(`[RabbitMQ] Error processing message from queue. ${queue}:`, error);
+          this.channel.nack(msg, false, true);
+        }
+      }
+    });
+  }
+
+  public async publishInQueue<T>(queue: string, message: T): Promise<void> {
+    if (!this.channel) {
+      throw new Error(`[RabbitMQ] Channel not initialized`);
+    }
+    await this.channel.assertQueue(queue, { durable: true });
     this.channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)), { persistent: true });
   }
 }
