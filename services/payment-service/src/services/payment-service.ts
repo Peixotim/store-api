4a9380b5085fac41da86008598d2338692269e69
Peixotim <pedropeixotovz@gmail.com>
2026-03-16 11:16:44 -0300

    feat: implementar serviço de pagamento com processamento e publicação de resultados

--- services/payment-service/src/services/payment-service.ts
+++ services/payment-service/src/services/payment-service.ts
@@ -1,1 +1,22 @@
-export class PaymentService {}
+import { RabbitMQService } from './rabbitmq-service';
+import { PaymentRepository } from '../repositories/payment-service';
+export class PaymentService {
+  constructor(private rabbitMQ: RabbitMQService) {}
+  private paymentRepo: PaymentRepository = new PaymentRepository();
+
+  public async processPayment(order: any) {
+    const isApproved = Math.random() > 0.2;
+
+    await this.paymentRepo.create({
+      order_id: order.order_id,
+      amount: order.total_price,
+      status: isApproved ? 'APPROVED' : 'DECLINED',
+      transaction_id: `TX-${Date.now()}`,
+    });
+
+    await this.rabbitMQ.publishInQueue('payment_finished', {
+      order_id: order.order_id,
+      status: isApproved ? 'PAID' : 'CANCELED',
+    });
+  }
+}
