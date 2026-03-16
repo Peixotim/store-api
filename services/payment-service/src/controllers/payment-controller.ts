3d21ff7d8d75a61be9f1c6884e02557e26473a21
Peixotim <pedropeixotovz@gmail.com>
2026-03-16 11:16:27 -0300

    feat: implementar controlador de pagamento com método de verificação de saúde

--- services/payment-service/src/controllers/payment-controller.ts
+++ services/payment-service/src/controllers/payment-controller.ts
@@ -1,4 +1,11 @@
 import { PaymentService } from '../services/payment-service';
+import { Request, Response } from 'express';
+
 export class PaymentController {
-  private paymentService = new PaymentService();
+  constructor(private paymentService: PaymentService) {}
+  public async health(req: Request, res: Response) {
+    return res.status(200).json({
+      message: `API IS RUNNING !`,
+    });
+  }
 }
